830b8af27b62901911b855af5158aa3eea5c64e4
Boba Fett <thomascupchurch@gmail.com>
2021-08-03 11:47:46 -0500

    use forEach

--- app.js
+++ app.js
@@ -14,9 +14,15 @@ console.log(profileDataArgs);
 
 
 const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
     }
+
+    console.log('============');
+
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
 };
 
 printProfileData(profileDataArgs);
