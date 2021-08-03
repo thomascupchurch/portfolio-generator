06c4f7293d1c3cfad271c1e86fa332866438217b
Boba Fett <thomascupchurch@gmail.com>
2021-08-03 16:20:19 -0500

    template literals

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const  [thomas, thomashub] = profileDataArgs;
 
-
-
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName} 
+    Github: ${githubName}
+    `;
+};
+console.log(thomas, thomashub);
+console.log(generatePage(name, github));
 
 
 
