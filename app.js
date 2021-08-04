e3a3ffe317ee14bab8a334c66e5e373609786ae7
Boba Fett <thomascupchurch@gmail.com>
2021-08-04 10:41:34 -0500

    add require fs

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
@@ -22,9 +24,12 @@ const generatePage = (userName, githubName) => {
     `;
 };
 
-console.log(name, github);
-console.log(generatePage(name, github));
 
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
 
 
 
