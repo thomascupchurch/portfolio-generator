16e6a0e8df06d57b91bcd84ecb88a3eff3c81b4a
Boba Fett <thomascupchurch@gmail.com>
2021-08-05 10:28:12 -0500

    add input validation

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
